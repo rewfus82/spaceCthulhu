8695a5cf9ee2090e9bf335ddf352f66b18ba191b
Ryan McLaughlin <rcmc82@gmail.com>
2023-03-02 12:07:16 -0600

    another push!

--- model/AiNPCModel/AiNPCModel.java
+++ model/AiNPCModel/AiNPCModel.java
@@ -3,54 +3,52 @@ package spaceCthulhu.model.AiNPCModel;
 import spaceCthulhu.model.enums.EnumBody;
 
 public class AiNPCModel {
+
+//Variables for the AiNPCModel class
+
     private String aiNPCName;
     private int aiNPCID;
-    //* a variable for the aiNPC's role that is defined by the aiNPC enum */
     private EnumBody.aiNPC aiNPCRole;
 
-    //* this set of variables represents the following AI NPC stats:
+    // this set of variables represents the following AI NPC stats:
     // Functionality (FCT): representing the AI's ability to perform their job as described by their programming.
     // Adaptability (ADP): representing the AI's ability to do and learn things not related to their programming.
     // Agility (AGL): representing the AI's ability to act and react dynamically.
     // Communication (COM): representing the AI's ability to communicate with other beings or machines effectively.
+    private int statFCT = 10;
+    private int statADP = 10;
+    private int statAGL = 10;
+    private int statCOM = 10;
 
-    private int aiNPCFCT;
-    private int aiNPCADP;
-    private int aiNPCAGL;
-    private int aiNPCCOM;
-    
+//Constructors for the AiNPCModel class
+
+    // Default constructor
     public AiNPCModel() {
     }
 
-// Constructor with only name and ID
-
+    // Constructor with only name and ID
     public AiNPCModel(String aiNPCName, int aiNPCID) {
         this.aiNPCName = aiNPCName;
         this.aiNPCID = aiNPCID;
-        this.setAiNPCStats(50, 50, 50, 50);
-
+        this.setStats(50, 50, 50, 50);
     }
 
-// Constructor with name, ID, and role
-
+    // Constructor with name, ID, and role
     public AiNPCModel(String aiNPCName, int aiNPCID, EnumBody.aiNPC aiNPCRole) {
         this.aiNPCName = aiNPCName;
         this.aiNPCID = aiNPCID;
         this.aiNPCRole = aiNPCRole;
-        this.setAiNPCStats(50, 50, 50, 50);
+        this.setStats(50, 50, 50, 50);
     }
 
-// Constructor with name, ID, role, and stats
-
+    // Constructor with name, ID, role, and stats
     public AiNPCModel(String aiNPCName, int aiNPCID, EnumBody.aiNPC aiNPCRole, int aiNPCFCT, int aiNPCADP, int aiNPCAGL, int aiNPCCOM) {
         this.aiNPCName = aiNPCName;
         this.aiNPCID = aiNPCID;
         this.aiNPCRole = aiNPCRole;
-        this.setAiNPCStats(aiNPCFCT, aiNPCADP, aiNPCAGL, aiNPCCOM);
+        this.setStats(aiNPCFCT, aiNPCADP, aiNPCAGL, aiNPCCOM);
 
     }
-
-    
 
 
 //Getters
@@ -67,24 +65,24 @@ public class AiNPCModel {
         return aiNPCRole;
     }
 
-    public int getAiNPCFCT() {
-        return aiNPCFCT;
+    public int getStatFCT() {
+        return statFCT;
     }
 
-    public int getAiNPCADP() {
-        return aiNPCADP;
+    public int getStatADP() {
+        return statADP;
     }
 
-    public int getAiNPCAGL() {
-        return aiNPCAGL;
+    public int getStatAGL() {
+        return statAGL;
     }
 
-    public int getAiNPCCOM() {
-        return aiNPCCOM;
+    public int getStatCOM() {
+        return statCOM;
     }
 
 //Setters
-
+    
     public void setAiNPCName(String aiNPCName) {
         this.aiNPCName = aiNPCName;
     }
@@ -96,75 +94,60 @@ public class AiNPCModel {
     public void setAiNPCRole(EnumBody.aiNPC aiNPCRole) {
         this.aiNPCRole = aiNPCRole;
     }
+
     
 // a function that sets the AI NPC stat FCT to the value passed in the parameter, but only if it is between 10 and 90
     public void setStatFCT(int aiNPCFCT) {
         try {
-            this.aiNPCFCT = Math.min(Math.max(aiNPCFCT, 10), 90);
+            this.statFCT = Math.min(Math.max(aiNPCFCT, 10), 90);
         } catch (Exception e) {
-            System.out.println("Error setting AI NPC FCT: " + e.getMessage());
+            this.statFCT = 10;
         }
     }
 
 // a function that sets the AI NPC stat ADP to the value passed in the parameter, but only if it is between 10 and 90
     public void setStatADP(int aiNPCADP) {
         try {
-            this.aiNPCADP = Math.min(Math.max(aiNPCADP, 10), 90);
+            this.statADP = Math.min(Math.max(aiNPCADP, 10), 90);
         } catch (Exception e) {
-            System.out.println("Error setting AI NPC ADP: " + e.getMessage());
+            this.statADP = 10;
         }
     }
 
 // a function that sets the AI NPC stat AGL to the value passed in the parameter, but only if it is between 10 and 90
     public void setStatAGL(int aiNPCAGL) {
         try {
-            this.aiNPCAGL = Math.min(Math.max(aiNPCAGL, 10), 90);
+            this.statAGL = Math.min(Math.max(aiNPCAGL, 10), 90);
         } catch (Exception e) {
-            System.out.println("Error setting AI NPC AGL: " + e.getMessage());
+            this.statAGL = 10;
         }
-    }
+    }   
 
 // a function that sets the AI NPC stat COM to the value passed in the parameter, but only if it is between 10 and 90
     public void setStatCOM(int aiNPCCOM) {
         try {
-            this.aiNPCCOM = Math.min(Math.max(aiNPCCOM, 10), 90);
+            this.statCOM = Math.min(Math.max(aiNPCCOM, 10), 90);
         } catch (Exception e) {
-            System.out.println("Error setting AI NPC COM: " + e.getMessage());
+            this.statCOM = 10;
         }
     }
 
 // a function that sets the AI NPC stats to the values passed in the parameters, but only if they are between 10 and 90
     public void setStats(int aiNPCFCT, int aiNPCADP, int aiNPCAGL, int aiNPCCOM) {
         try {
-            this.aiNPCFCT = Math.min(Math.max(aiNPCFCT, 10), 90);
-            this.aiNPCADP = Math.min(Math.max(aiNPCADP, 10), 90);
-            this.aiNPCAGL = Math.min(Math.max(aiNPCAGL, 10), 90);
-            this.aiNPCCOM = Math.min(Math.max(aiNPCCOM, 10), 90);
+            this.setStatFCT(aiNPCFCT);
+            this.setStatADP(aiNPCADP);
+            this.setStatAGL(aiNPCAGL);
+            this.setStatCOM(aiNPCCOM);
         } catch (Exception e) {
             System.out.println("Error setting AI NPC stats: " + e.getMessage());
         }
     }
-    
-
-    
-    
-
-
 
     @Override
-
     public String toString() {
-        return "AiNPCModel{" +
-                "aiNPCName='" + aiNPCName + '\'' +
-                ", aiNPCID=" + aiNPCID +
-                ", aiNPCRole=" + aiNPCRole +
-                ", aiNPCFCT=" + aiNPCFCT +
-                ", aiNPCADP=" + aiNPCADP +
-                ", aiNPCAGL=" + aiNPCAGL +
-                ", aiNPCCOM=" + aiNPCCOM +
-                '}';
-    }
-
+        return "AiNPCModel{" + "aiNPCName=" + aiNPCName + ", aiNPCID=" + aiNPCID + ", aiNPCRole=" + aiNPCRole + ", statFCT=" + statFCT + ", statADP=" + statADP + ", statAGL=" + statAGL + ", statCOM=" + statCOM + '}';
+    }   
 
 
 
